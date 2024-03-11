f598bd28bfa7bbc6d3ca203e44d3a7c086189c32
gabmora <gabimm29@gmail.com>
2024-03-10 20:43:57 -0400

    heating page

--- src/components/Heating.js
+++ src/components/Heating.js
@@ -8,6 +8,8 @@ function Heating() {
     return (
         <>
         <h5>heating</h5>
+        <h1>we fix heat stuff too.</h1>
+        
         <img className="frontpageimage" src={heatingImage} alt="Heating" />
         
         
