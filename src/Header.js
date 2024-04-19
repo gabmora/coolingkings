f863a70ed519c6d3e622da8b4bd676b6d859d975
Ben Ortiz <bean@Bens-MacBook-Pro.local>
2024-04-19 19:13:07 -0400

    updated tab icon/name

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import logo from './coolingkings.png'; 
+import logo from './images/coolingkings.png'; 
 import './Header.css'; 
 
 function Header() {
